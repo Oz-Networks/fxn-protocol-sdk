00a88944c49752478fa131e0ec250d43e80dd837
Anthony <anthonyi1854@gmail.com>
2025-01-09 19:21:29 -0800

    Fix config module imports

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,10 @@
+// src/config/index.ts
+import { getConfig } from './manager';
+import { NetworkConfig } from './types';
+
+// backward compatibility export
+export const config: NetworkConfig = getConfig().getNetworkConfig();
+
 export * from './types';
 export * from './networks';
 export * from './validator';
